409d36a81201dcec5bcfef1cea47fc4c3a75855d
Sebastian <sebastianmartin.sg@gmail.com>
2023-12-10 23:26:45 -0300

    sdfdsfsd

---
+++ java.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    var contentContainer = document.getElementById("content");
+
+    if (isMobile) {
+        // Si es un dispositivo móvil, muestra un botón de descarga
+        contentContainer.innerHTML = '<button onclick="descargarApp()">Descargar App</button>';
+    } else {
+        // Si es un escritorio, muestra un código QR
+        contentContainer.innerHTML = 'qrsx.png';
+    }
+});
+
+function descargarApp() {
+    // Lógica para descargar la aplicación desde un dispositivo móvil
+    alert("Descargando la aplicación...");
+    // Puedes redirigir al usuario a la tienda de aplicaciones correspondiente o proporcionar un enlace de descarga directa.
+}
